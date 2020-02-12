7ac39ff8465561ae1bec5ab0bfebaf221b52a7d9
ikerpedrejon@gmail.com <ikerpedrejon@gmail.com>
2020-02-12 10:10:37 +0100

    Inicio de la Aplicación Angular y Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GLOBAL } from './global';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'Pokemon-Cliente-Angular';
-}
+  // Variable
+  title : string;
+  global : Array<any>;
+
+  constructor() { 
+    console.trace('constructor AppComponent');
+	  this.title = 'Pokemon-Cliente-Angular';
+	  this.global = GLOBAL;
+  } // Constructor
+
+  ngOnInit() {
+    console.trace('ngOnInit FiltrosComponent');
+  } // ngOnInit
+} // AppComponent
---
+++ src/app/global.ts
@@ -0,0 +1,6 @@
+export const GLOBAL = [{
+    'usuario': 'PRIYI00',
+    'nombre': 'Iker Pedrejon Galan',
+    'github': 'https://github.com/PRIYI00/Pokemon-Cliente-Angular',
+    'version': '0.1'
+}]; 
